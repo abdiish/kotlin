d95ea3696887260bd104f298482f91266c77241c
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-17 18:47:44 -0500

    Uso de constantes en kotlin

---
+++ 05-Constantes.kt
@@ -0,0 +1,16 @@
+
+const val direccion = "17 de Junio" 
+
+fun main(){
+    
+    println(direccion)
+}
+
+class Constantes{
+
+    companion object{
+
+        const val direccion = "17 de Junio 2021"
+        
+    }
+}
