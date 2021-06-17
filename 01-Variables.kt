8e4de52d8305c20cae96e6ce59c614789a5bb134
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-17 18:09:37 -0500

    Variables en Kotlin, mutables e inmutables, así cmo el tipo de inferencia

---
+++ 01-Variables.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+    //Java, variables mutables 
+    Int a = 2;
+    Int b = 3;
+    Int c =  a + b;
+
+    //kotlin
+    var a:Int = 2
+    var b:Int = 3
+    var c:Int = a + b
+
+    //Inferencia de tipo de variable, kotlin infiere en que el tipo de dato es flotante
+    
+    var x = 3.2
+
+    //Se usa var para indicar que la variable es mutable
+
+    //Se usa val para variables de tipo inmutable
+    
+    val x = 2
+
+}
