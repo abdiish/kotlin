07f927f21d12092334e55e2eac7ca1e0e039184f
Alan Cortes <ing.ti.upmh@gmail.com>
2021-06-17 19:20:28 -0500

    Al declarar una función de hace uso de la palabra reservada fun y se indica el tipo de valor que debe retornar

---
+++ 07-Funciones.kt
@@ -0,0 +1,17 @@
+
+fun main() {
+
+    //Llamada a la función
+    println(esPar(2))
+
+}
+
+//Usar fun para declarar una funcion
+//Retornara un tipo de dato Boolean
+//Se puden declarar del tipo private
+
+fun esPar(numero: Int) : Boolean {
+
+    return numero % 2 == 0
+
+}
